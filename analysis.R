a7686faeecfff33e5235732e780f95b8b020151f
mjevans26 <evans.michael26@gmail.com>
2017-07-21 17:07:00 -0400

    initial commit

---
+++ analysis.R
@@ -0,0 +1,47 @@
+library(plotly)
+library(lme4)
+
+plot_ly(data = portal_raw[portal_raw$CharacteristicName == "Mercury" & portal_raw$ResultSampleFractionText == "Total",])%>%
+  add_trace(type = "scatter", mode = "markers", color = ~ MonitoringLocationIdentifier,
+            x = ~Mined, y = ~ResultMeasureValue,
+            name = 'Cadmium')
+
+plot_ly(data = portal_raw[portal_raw$CharacteristicName == "pH" &
+                            portal_raw$MonitoringLocationIdentifier == "11NPSWRD-CUGA_CPSU_RR",])%>%
+  add_trace(type = "scatter", mode = "markers",
+            x = ~Date, y = ~ResultMeasureValue,
+            name = 'pH')
+
+hist(portal_raw$ResultMeasureValue[portal_raw$CharacteristicName == 'pH'])
+hist(portal_raw$ResultMeasureValue[portal_raw$CharacteristicName == 'Cadmium' & portal_raw$ResultMeasureValue < 1])
+hist(portal_raw$ResultMeasureValue[portal_raw$CharacteristicName == 'Copper'])
+hist(portal_raw$ResultMeasureValue[portal_raw$CharacteristicName == 'Lead'])
+hist(portal_raw$ResultMeasureValue[portal_raw$CharacteristicName == 'Mercury'])
+hist(portal_raw$ResultMeasureValue[portal_raw$CharacteristicName == 'Turbidity'])
+hist(portal_raw$ResultMeasureValue[portal_raw$CharacteristicName == 'Total suspended solids'])
+
+simple_plot <- function(character, measure){
+  plot_ly(data = portal_raw[portal_raw$CharacteristicName == character & portal_raw$ResultSampleFractionText == measure,])%>%
+    add_trace(type = "scatter", mode = "markers",
+              x = ~pMine, y = ~Values,
+              name = ~paste(measure, character))
+}
+
+simple_plot("Cadmium", "Dissolved")
+
+time_plot <- function(character, measure, min){
+  sts <- sites$MonitoringLocationIdentifier[sites$count > min]
+  c1 <- portal_raw$MonitoringLocationIdentifier %in% sts
+  c2 <- portal_raw$CharacteristicName == character
+  c3 <- portal_raw$ResultSampleFractionText == measure
+  
+  plot_ly(data = portal_raw[c1 & c2 & c3,])%>%
+    add_trace(type = "scatter", mode = "markers",
+              x = ~pMine, y = ~Values, color = ~ MonitoringLocationIdentifier,
+              name = ~paste(measure, character), visible = "legendonly",
+              text = ~paste("Condition:", HydrologicCondition, "<br>Event:", HydrologicEvent),
+              hoverinfo = "text")
+}
+
+lmer(data = portal_raw[portal_raw$MonitoringLocationIdentifier %in% sites$MonitoringLocationIdentifier[sites$count > 10],], 
+     ResultMeasureValue ~ 1|MonitoringLocationIdentifier + pMine + HydrologicCondition)
---
+++ processing.R
@@ -0,0 +1,71 @@
+portal_raw <- readRDS("GISresults.rds")
+sites <- readRDS("GISsites.rds")
+
+portal_raw$ResultMeasure.MeasureUnitCode <- trimws(portal_raw$ResultMeasure.MeasureUnitCode)
+portal_raw$Date <- as.Date(portal_raw$ActivityStartDate, format = "%m/%d/%Y")
+portal_raw$ResultMeasureValue <- as.character(portal_raw$ResultMeasureValue)
+portal_raw$ResultMeasureValue[portal_raw$ResultMeasureValue == "<10"] <- as.character(runif(length(portal_raw$ResultMeasureValue[portal_raw$ResultMeasureValue == "<10"]), 0, 9))
+portal_raw$ResultMeasureValue[portal_raw$ResultMeasureValue == "U"]<-""
+portal_raw$ResultMeasureValue <- as.numeric(portal_raw$ResultMeasureValue)
+
+min(as.numeric(portal_raw$ResultMeasureValue[portal_raw$CharactersitcName == "Cadmium" 
+                                  & portal_raw$ResultMeasure.MeasureUnitCode == "mg/l"]))
+
+#result of spatial join in arc between mining footprints and monitoring site catchment basins.
+#contains all combinations of mining footprints and sampling site basins
+
+join_data <- read.csv("Footprint_Sites_Join.csv", header = TRUE, sep = ",")
+#erroneously assigned GRIDCODE == 0 due to duplicated site basins from watershed trials
+join_data$GRIDCODE[join_data$FID %in% c(31959, 35869, 39753, 43368, 47150, 51059, 54844, 54849)] <- 639
+join_data$DATE <- as.Date(as.character(join_data$YEAR), format = "%Y")
+site_areas <- group_by(join_data, YEAR, GRIDCODE, STATION_ID)%>%
+  summarise(DATE = first(DATE), MINED = sum(SUM), ORG = first(ORG), NAME = first(NAME))
+
+
+
+#match the area of mining activities within a sampling site's basin for the appropriate year, 
+#based on when the sample was taken
+portal_raw$Mined <- sapply(1:nrow(portal_raw), function(i){
+  a <- site_areas$MINED[as.character(site_areas$STATION_ID) == as.character(portal_raw$MonitoringLocationIdentifier[i]) &
+                   lubridate::year(portal_raw$Date[i]) - site_areas$YEAR == 1]
+  if(length(a) == 0){a <- 0}
+  return(a)
+}, USE.NAMES = FALSE
+)
+
+#going back to join total area of site drainage basins
+temp <- read.table("Footprint_Sites_Join.txt", header = TRUE)
+#erroneous duplicates from trials of watershed analysis
+temp <- temp[-c(3,4054),]
+#some basins are multipart if connected by corners
+temp <- group_by(temp, GRIDCODE)%>%summarise(AREA = sum(AREA))
+site_areas <- left_join(site_areas, temp, by = "GRIDCODE")
+rm(temp)
+
+portal_raw$Area <- sapply(1:nrow(portal_raw), function(i){
+  a <- site_areas$AREA[as.character(site_areas$STATION_ID) == as.character(portal_raw$MonitoringLocationIdentifier[i]) &
+                         lubridate::year(portal_raw$Date[i]) - site_areas$YEAR == 1]
+  if(length(a) == 0){a <- 0}
+  return(a)
+}, USE.NAMES = FALSE
+)
+
+portal_raw$pMine <- portal_raw$Mined/portal_raw$Area
+
+#convert all concentration values to ug/l
+portal_raw$Values <- test<- sapply(1:nrow(portal_raw), function(i){
+  if(portal_raw$ResultMeasure.MeasureUnitCode[i] == "mg/l"){
+    ug <- portal_raw$ResultMeasureValue[i] * 1000
+  }else if (portal_raw$ResultMeasure.MeasureUnitCode[i] == "ng/l"){
+    ug <- portal_raw$ResultMeasureValue[i] * 0.001
+  }else {
+    ug <- portal_raw$ResultMeasureValue[i]
+  }
+  return(ug)
+}, USE.NAMES = FALSE)
+
+#create counts of stations
+site_counts <- group_by(portal_raw, MonitoringLocationIdentifier, ActivityStartDate)%>%
+  group_by(MonitoringLocationIdentifier)%>%summarise(count = n())
+
+sites <- merge(sites, site_counts, by = "MonitoringLocationIdentifier", all = TRUE)
